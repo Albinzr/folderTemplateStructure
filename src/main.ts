abff89925101d55d7cb0e8284ca4727b43aa9e8b
WingsJ <mail@wings-j.cn>
2021-01-29 22:01:58 +0800

    update

--- src/main.ts
+++ src/main.ts
@@ -6,14 +6,34 @@
 
 import * as vscode from 'vscode'
 
+enum Type {
+  file,
+  dir
+}
+
+/**
+ * @name 模板
+ */
+interface Template {
+  title: string
+  name: string
+  type: Type
+  children?: Template[]
+}
+
 /* public */
 
 /**
  * @name 主函数
  * @param target 目标
  */
-function main(target: { fsPath: string }): void {
+async function main(target: { fsPath: string }): Promise<void> {
   let templates = vscode.workspace.getConfiguration('createFolderFromTemplate').templates
+
+  let titles = templates.map((a: Template) => a.title)
+  let select = await vscode.window.showQuickPick(titles, { placeHolder: '选择模板' })
+
+  let template = templates.find((a: Template) => a.title === select)
 }
 
 /* construct */
